3f6c0f0f6d7ea2e3b1c03993f67b2a015485ebab
Yucheng Liu <liu.r.yucheng@gmail.com>
2021-08-01 02:03:22 +0800

    Fixed crop image size limit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import setuptools
 def main():
     setuptools.setup(
         name="aidesign-utils",
-        version="0.3.0",
+        version="0.3.1",
         description="AI Design utils application.",
         author="AI Design Team",
         packages=setuptools.find_packages(),
